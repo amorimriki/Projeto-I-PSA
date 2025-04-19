a66b6dd94f9b815a65f2713e6484276b39bb229f
amorimriki <165579277+amorimriki@users.noreply.github.com>
2025-04-19 16:39:07 +0100

    Atualização da informação dos modelos

--- PSA_APP/frontend/src/components/React_HomePage.jsx
+++ PSA_APP/frontend/src/components/React_HomePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Button, Row, Col, Card, Carousel } from "react-bootstrap";
+import { Container, Button, Carousel } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../App.css";
 
